3a421728d612c2949defbadfbf7123b91a2a25e1
nh-g <giangngohong@gmail.com>
2021-11-13 17:23:52 +0100

    add SearchBar

---
+++ src/components/shared/SearchBar.tsx
@@ -0,0 +1,30 @@
+//NPM Packages
+import { useState, FC } from "react";
+import { useHistory } from "react-router-dom";
+
+//Local Files
+import loupe from "assets/img/loupe.png";
+
+const SearchBar: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const history = useHistory();
+  return (
+    <div className={isOpen ? "searchbar-open" : "searchbar"}>
+      <button onClick={() => setIsOpen(!isOpen)}>
+        <img src={loupe} alt="" />
+      </button>
+      {isOpen && (
+        <input
+          type="text"
+          placeholder="Titles"
+          onChange={(e) => {
+            history.push(`/search/${e.target.value}`);
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default SearchBar;
