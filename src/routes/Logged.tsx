7529596a757fecc51843cfb2c575fd8cb1893798
nh-g <giangngohong@gmail.com>
2021-11-17 15:21:50 +0100

    add route constants

--- src/routes/Logged.tsx
+++ src/routes/Logged.tsx
@@ -1,8 +1,17 @@
-//@ts-nocheck
+// NPM packages
 import { Route } from "react-router-dom";
 
 // Project files
 import { useAuth } from "state/AuthProvider";
+import {
+  ROUTE_LOGIN,
+  ROUTE_SIGNUP,
+  ROUTE_BROWSE,
+  ROUTE_SEARCH,
+  ROUTE_FILTER,
+  ROUTE_ADMIN,
+} from "constants/routes";
+
 import Browse from "pages/browse";
 import Login from "pages/authentication/Login";
 import SignUp from "pages/authentication/Signup";
@@ -17,13 +26,20 @@ export default function Logged() {
   return (
     <>
       <HeaderMenu />
-      <Route exact path="/browse" component={Browse} />
-      <Route component={Login} path="/login" />
-      <Route component={SignUp} path="/signup" />
-      <Route component={Search} exact path="/search/" />
-      <Route component={Search} path="/search/:query" />
-      <Route component={Genre} path="/genre/:genreName" />
-      {user.role === "admin" && <Route component={Admin} path="/admin" />}
+      {/* <Route component={Login} path="/login" /> */}
+      {user.role === "admin" && <Route path={ROUTE_ADMIN} component={Admin} />}
+
+      <Route exact path={ROUTE_BROWSE} component={Browse} />
+
+      <Route component={Search} path={`${ROUTE_SEARCH}/:query`} />
+      <Route component={Genre} path={`${ROUTE_FILTER}/:genreName`} />
+
+      <Route path={ROUTE_LOGIN} component={Login} />
+      <Route path={ROUTE_SIGNUP} component={SignUp} />
+
+      {/* <Route exact path="/browse" component={Browse} /> */}
+      {/* <Route component={Login} path="/login" />
+      <Route component={SignUp} path="/signup" /> */}
     </>
   );
 }
