6b76d6287b2429038450377cb9c38944f3452be6
nh-g <giangngohong@gmail.com>
2021-11-17 15:31:47 +0100

    add ternary operator

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ROUTE_BROWSE, ROUTE_HOME } from '../../constants/routes';
+import { ROUTE_BROWSE, ROUTE_HOME } from "../../constants/routes";
 import { useAuth } from 'state/AuthProvider';
 import mergeClassNames from '../../utils/merge-class-names';
 import './Logo.css';
 
 const Logo = ({ ...rest }) => {
-  // const { user } = useAuth();
+  const { user } = useAuth();
 
   return (
-    // <Link to={user ? ROUTE_BROWSE : ROUTE_HOME}>
-    <Link to={ROUTE_HOME}>
+    <Link to={user.role = "" ? ROUTE_BROWSE : ROUTE_HOME}>
       <svg
         viewBox="0 0 111 30"
         focusable="false"
